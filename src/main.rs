f99c06af88ba6632ea3e3e41d88eec15e3b75349
idkjanevim <ondatra@centrum.cz>
2024-02-02 18:21:10 +0100

    Basic comments

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,21 @@ use log::info;
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
-
+// Binding interupts
 bind_interrupts!(struct Irqs {
+    // cyw43 interupt
     PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<PIO0>;
+    // USB interupt
     USBCTRL_IRQ => embassy_rp::usb::InterruptHandler<USB>;
 });
 
+// Task for cyw43 communication
 #[embassy_executor::task]
 async fn wifi_task(runner: cyw43::Runner<'static, Output<'static, PIN_23>, PioSpi<'static, PIN_25, PIO0, 0, DMA_CH0>>) -> ! {
     runner.run().await
 }
 
+// Task for usb logging
 #[embassy_executor::task]
 async fn logger_task(driver: Driver<'static, USB>) {
     embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
@@ -32,24 +36,28 @@ async fn logger_task(driver: Driver<'static, USB>) {
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    // Nahrajeme firmware pro cyw43
+    
+    // Including firmware
     let fw = include_bytes!("../firmware/43439A0.bin");
     let clm = include_bytes!("../firmware/43439A0_clm.bin");
 
+    // cyw43 comunication setup
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
     let mut pio = Pio::new(p.PIO0, Irqs);
     let spi = PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, p.PIN_24, p.PIN_29, p.DMA_CH0);
-
-    let driver = Driver::new(p.USB, Irqs);
-    spawner.spawn(logger_task(driver)).unwrap();
-
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
     let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
     spawner.spawn(wifi_task(runner)).unwrap();
 
+    // USB logging setup
+    let driver = Driver::new(p.USB, Irqs);
+    spawner.spawn(logger_task(driver)).unwrap();
+
+    // Initialize wifi driver
     control.init(clm).await;
+    // Power management
     control
         .set_power_management(cyw43::PowerManagementMode::PowerSave)
         .await;
